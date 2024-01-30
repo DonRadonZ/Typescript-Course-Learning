727a899d7ac43ece729e3e0c55dadecaba608ecc
Supachai Sinkaseam <supachai27102000@gmail.com>
2024-01-31 00:39:13 +0700

    Type Castion

--- 05-Advance-Types/src/app.ts
+++ 05-Advance-Types/src/app.ts
@@ -104,3 +104,12 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({ type: 'bird', flyingSpeed: 10 });
+
+//Type Casting
+
+const userInputElement = document.getElementById('user-input') as HTMLInputElement;
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input');
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = 'Hi there!';
+}
