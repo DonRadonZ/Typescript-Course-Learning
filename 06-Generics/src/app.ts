d914e2ec7fd4f5170fc225c1b277ff0fc44e2881
Supachai Sinkaseam <supachai27102000@gmail.com>
2024-01-31 23:12:02 +0700

    update app.ts Generic

--- 06-Generics/src/app.ts
+++ 06-Generics/src/app.ts
@@ -15,7 +15,7 @@
 // Creating a Generic Function
 
 function merge<T extends object,U extends object>(objA: T, objB: U) {
-    return Object.assign({},objA, objB);
+    return Object.assign( objA,objB)
 }
 
 const mergeObj = merge<{name: string, hobbies: string[]}, {age: number}>({name: 'Don',hobbies: ['Sports'] }, {age: 23});
