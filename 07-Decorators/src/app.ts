cace46725f9c42c920e4d1c930f9f1f9e183dd0f
RadonZ <supachai27102000@gmail.com>
2024-02-01 14:29:47 +0700

    add new code

--- 07-Decorators/src/app.ts
+++ 07-Decorators/src/app.ts
@@ -12,20 +12,26 @@ function Logger(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
     console.log('TEMPLATE FACTORY');
-    return function (constructor: any) {
-        console.log('Rendering template')
+    return function<T extends {new(...args: any[]): {name: string}} > (originalConstructor: T) {    
+     // Returning (and changing) a Class in a Class Decorator
+        return class extends originalConstructor {
+            constructor(..._: any[]) {
+                super();
+                console.log('Rendering template')
         const hookEl = document.getElementById(hookId);
-        const p = new constructor();
         if (hookEl) {
             hookEl.innerHTML = template;
-            hookEl.querySelector('h1')!.textContent = p.name;
+            hookEl.querySelector('h1')!.textContent = this.name;
         }
     }
+  }
+ }
 }
+
 
 @WithTemplate('<h1>My Preson Object</h1>', 'app')
 
-    // @Logger('LOGGING - PERSON')
+// @Logger('LOGGING - PERSON')
 
 //Adding Multiple Decorators
 @Logger('LOGGING')
@@ -40,3 +46,70 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+// ---
+
+function Log(target: any, propertyName: string | Symbol) {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
+}
+
+// Accessor & Parameter Decorators
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor){
+    console.log('Accessor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log3(
+    target: any, 
+    name: string | Symbol, 
+    descriptor: PropertyDescriptor
+)   {
+    console.log('Accessor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+
+
+class Product {
+    @Log
+    title: string;
+    private _price: number;
+    
+    @Log2
+    set price(val: number){
+        if (val > 0) {
+        this._price = val;
+        } else {
+            throw new Error('Invalid price - should be positive!')
+        }
+    }
+    
+    constructor(t: string, p: number){
+        this.title = t;
+        this._price = p;
+    }
+
+    @Log3
+    getPriceWithTax(@Log4 tax: number){
+        return this._price * (1 + tax);
+    }
+}
+
+// When Do Decorators Execute?
+const p1 = new Product('Book', 19);
+const p2 = new Product('Book 2', 29)
+
+
+
