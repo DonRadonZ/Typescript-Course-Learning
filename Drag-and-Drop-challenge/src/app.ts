3283320214d777eedeb23b2a06a3c0b333c3ae73
Supachai Sinkaseam <supachai27102000@gmail.com>
2024-02-03 23:54:09 +0700

    Drag and Drop and Update code style

--- Drag-and-Drop-challenge/src/app.ts
+++ Drag-and-Drop-challenge/src/app.ts
@@ -1,22 +1,75 @@
-// Autobind Decorator
-function Autobind(
-    _: any,
-    _2: string,
-    descriptor: PropertyDescriptor
-) {
-    const originalMethod = descriptor.value;
-    const adjDescriptor: PropertyDescriptor = {
-        configurable: true,
-        get() {
-            const boundFn = originalMethod.bind(this);
-            return boundFn
+// Drag & Drop Interface
+interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHandler(event: DragEvent): void;
+}
+interface DragTarget {
+    dragOverHandler(event: DragEvent): void; 
+    dropHandler(event: DragEvent): void; 
+    dragLeaveHandler(event: DragEvent): void; 
+}
+
+// Work Type
+enum WorkStatus {
+    Active,
+    Finished
+}
+class Work{
+    constructor(
+        public id: string,
+        public title: string,
+        public description: string,
+        public people: number,
+        public status: WorkStatus
+    ) { }
+}
+
+// Work State Management
+type Listener<T> = (items: T[]) => void;
+
+class State<T> {
+    protected listeners: Listener<T>[] = [];
+
+    addListener(listenerFn: Listener<T>) {
+        this.listeners.push(listenerFn);
+    }
+}
+
+class WorkState extends State<Work> {
+    private works: Work[] = [];  
+    private static instance: WorkState;
+
+    private constructor() {
+        super();
+    }
+
+    static getInstance() {
+        if (this.instance) {
+            return this.instance;
         }
-    };
-    return adjDescriptor;
-  }
+        this.instance = new WorkState();
+        return this.instance;
+    }
+    
+    
+    addWork(title: string, description: string, numOfPeople: number) {
+        const newWork = new Work(
+            Math.random().toString(),
+            title,
+            description,
+            numOfPeople,
+            WorkStatus.Active
+        );
+        this.works.push(newWork);
+        for (const listenerFn of this.listeners) {
+          listenerFn(this.works.slice());
+        }
+    }
+}
+
+const workState = WorkState.getInstance(); 
 
 // Validator
-
 interface Validatable {
     value: string | number;
     required?: boolean;
@@ -49,22 +102,24 @@ function validation(ValidationInput: Validatable) {
     return isValid
 }
 
-interface ValidatorConfig {
-    [property: string]: {
-        [validatableProp: string]: string[] //['required', 'positive']
-    }
-}
-
-const registeredValidators: ValidatorConfig = {}  
-
-function PositiveNumber(target: any, propName: string) {
-    registeredValidators[target.constructor.name] = {
-        ...registeredValidators[target.constructor.name],
-        [propName]: ['positive']
+// Autobind Decorator
+function Autobind(
+    _: any,
+    _2: string,
+    descriptor: PropertyDescriptor
+) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn
         }
-    }
+    };
+    return adjDescriptor;
+  }
 
-abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+  abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     templateElement: HTMLTemplateElement;
     hostElement: T;
     element: U;
@@ -99,9 +154,50 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 
 }
 
-// Information class
-class WorkList extends Component<HTMLDivElement, HTMLElement>{
-    assignedWorks: Works[]
+
+// WorkItem Class
+class WorkItem extends Component<HTMLUListElement, HTMLLIElement>
+    implements Draggable{
+    private work: Work;
+    get persons() {
+        if (this.work.people === 1) {
+            return '1 person';
+        } else {
+            return `${this.work.people} persons`;
+        }
+    }
+    constructor(hostId:string, work:Work) {
+        super('single-project', hostId, false, work.id);
+        this.work = work;
+
+        this.configure();
+        this.renderContent();
+    }
+    
+    @Autobind
+    dragStartHandler(event: DragEvent) {
+        console.log(event);  
+    }
+    dragEndHandler(_: DragEvent) {
+        console.log('DragEnd');
+    }
+    configure() {
+        this.element.addEventListener('dragstart', this.dragStartHandler);
+        this.element.addEventListener('dragend', this.dragEndHandler);
+    }
+    
+    renderContent() {
+        this.element.querySelector('h2')!.textContent = this.work.title;
+        this.element.querySelector('h3'
+        )!.textContent = this.persons + 'Person assigned';
+        this.element.querySelector('p')!.textContent = this.work.description;
+    }
+}
+
+// WorkList Class
+class WorkList extends Component<HTMLDivElement, HTMLElement> implements
+    DragTarget{
+    assignedWorks: Work[]
 
     constructor(private type: 'active' | 'finished') {
         super('project-list','app', false, `${type}-projects`);
@@ -111,8 +207,27 @@ class WorkList extends Component<HTMLDivElement, HTMLElement>{
         this.renderContent();
     }
 
+    @Autobind
+    dragOverHandler(_: DragEvent) {
+        const listEl = this.element.querySelector('ul')!;
+        listEl.classList.add('droppable');
+    }
+
+    dropHandler(_: DragEvent) {
+        
+
+    }
+
+    dragLeaveHandler(_: DragEvent) {
+        const listEl = this.element.querySelector('ul')!;
+        listEl.classList.remove('droppable');
+    }
+
     configure() {
-        workState.addListener((works: Works[]) => {
+        this.element.addEventListener('dragover', this.dragOverHandler);
+        this.element.addEventListener('dragleave', this.dragLeaveHandler);
+        this.element.addEventListener('drop', this.dropHandler);
+        workState.addListener((works: Work[]) => {
             const relevantWorks = works.filter(wk => {
                 if (this.type === 'active') {
                     return wk.status === WorkStatus.Active;
@@ -128,88 +243,28 @@ class WorkList extends Component<HTMLDivElement, HTMLElement>{
         const listId = `${this.type}-projects-list`;
         this.element.querySelector('ul')!.id = listId;
         this.element.querySelector('h2')!.textContent =
-            this.type.toUpperCase() + ' PROJECTS';
+            this.type.toUpperCase() + ' WORKS';
         
     }
 
     private renderWorks() {
-        const listEl = <HTMLUListElement>document.getElementById(`${this.type}-projects-list`);
+        const listEl = document.getElementById(
+            `${this.type}-projects-list`
+        )! as HTMLUListElement;
         listEl.innerHTML = '';
         for (const wkItem of this.assignedWorks) { 
-            const listItem = document.createElement('li');
-            listItem.textContent = wkItem.title;
-            listEl.appendChild(listItem);
+            new WorkItem(this.element.querySelector('ul')!.id, wkItem)
         }
     }
 
-
-
 }
 
-// Work Type
-
-enum WorkStatus {Active, Finished}
-class Works{
-    constructor(
-        public id: string,
-        public title: string,
-        public description: string,
-        public people: number,
-        public status: WorkStatus
-    ) { }
-}
-
-// Work State Management
-type Listener<T> = (items: T[]) => void;
-
-class State<T> {
-    protected listeners: Listener<T>[] = [];
-
-    addListener(listenerFn: Listener<T>) {
-        this.listeners.push(listenerFn);
-    }
-}
-class WorkState extends State<Works> {
-    
-    private works: Works[] = [];  
-    private static instance: WorkState;
-
-    private constructor() {
-        super();
-    }
-
-    static getInstance() {
-        if (this.instance) {
-            return this.instance;
-        }
-        this.instance = new WorkState();
-        return this.instance;
-    }
-    
-    
-    addWork(title: string, description: string, numOfPeople: number) {
-        const newWork = new Works(
-            Math.random().toString(),
-            title,
-            description,
-            numOfPeople,
-            WorkStatus.Active
-        );
-        this.works.push(newWork);
-        for (const listenerFn of this.listeners) {
-          listenerFn(this.works.slice());
-        }
-    }
-}
-
-const workState = WorkState.getInstance(); 
 
 // Input Class
 class WorkInput extends Component<HTMLDivElement, HTMLFormElement> {
     titleInputElement: HTMLInputElement;
     descriptionInputElement: HTMLInputElement;
     peopleInputElement: HTMLInputElement;
-    // submitElement: HTMLButtonElement;
 
     constructor() { 
         super('project-input', 'app', true, 'user-input');
