0dbcf53338d19f7475a7be710b01970d83fd569c
Supachai Sinkaseam <supachai27102000@gmail.com>
2024-02-04 16:09:19 +0700

    remove unused path

--- 08-Modules-and-Namespaces/src/app.ts
+++ 08-Modules-and-Namespaces/src/app.ts
@@ -1,24 +1,9 @@
-/// <reference path="models/drag-drop.ts"/>
-/// <reference path="models/work.ts"/>
-/// <reference path="state/work-state.ts"/>
-/// <reference path="util/validation.ts"/>
-/// <reference path="decorators/autobind.ts"/>
 /// <reference path="components/work-input.ts"/>
 /// <reference path="components/work-list.ts"/>
 
 
 
 namespace App{
-
-
-
-
-
-
-
-
-
-
 new WorkInput();
 new WorkList('active');
 new WorkList('finished');
