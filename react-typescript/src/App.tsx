fc45669bb8a19669612a20d7f16fbb1e4f620d87
RadonZ <supachai27102000@gmail.com>
2024-02-05 15:03:38 +0700

    finish react-typescript

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import { Route } from 'react-router-dom';
 
 import TodoList from './components/TodoList'
 import NewTodo from './components/NewTodo';
+import {Todo} from './todo.model';
 import './App.css';
 
+
 const App: React.FC = () => {
-  const todos = [{id:'t1', text: 'Finish the course'}];
+  const [todos,setTodos] = useState<Todo[]>([]);
+  
   const addTodoHandler = (text: string) =>{
-    console.log(text)
+    setTodos(prevTodos => [
+      ...prevTodos,
+      {id: Math.random().toString(), text: text}
+    ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    });
   }
 
   return (
     <div className="App">
       <NewTodo onAddTodo={addTodoHandler}/>
-      <TodoList items={todos}/>
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler}/>
+  
     </div>
   );
 };
--- react-typescript/src/components/TodoList.tsx
+++ react-typescript/src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import './TodoList.css'
 
 
 interface TodoListProps {
@@ -7,13 +8,18 @@ interface TodoListProps {
         id: string,
         text: string
     }[];
+    onDeleteTodo: (id: string) => void;
 }
 
 const TodoList: React.FC<TodoListProps> = (props) => {
     
-    return (<ul>
+    return (
+    <ul>
         {props.items.map(todo => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+            <span>{todo.text}</span>
+            <button onClick={props.onDeleteTodo.bind(null, todo.id)}>DELETE</button>
+        </li>
         ))}
     </ul>
     );
